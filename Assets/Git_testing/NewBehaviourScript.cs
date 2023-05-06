a01db80303d8d7d9102064921fe0d65e3287bafa
Neutron <wbuczko@edu.cdv.pl>
2023-05-06 15:07:57 +0200

    glucio

--- Assets/Git_testing/NewBehaviourScript.cs
+++ Assets/Git_testing/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //glucio
     }
 
     // Update is called once per frame
